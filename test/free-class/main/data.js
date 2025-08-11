93c96854c42d0bdb111a91cb655c26a1f82e9dc4
roc-chtai <roc.chtai@gmail.com>
2025-08-11 17:06:00 +0800

    Update data.js

--- test/free-class/main/data.js
+++ test/free-class/main/data.js
@@ -1,37 +1,30 @@
-// lectures-data.js
-window.LECTURES_DATA = {
-  latest: [
-    {
-      title: "AI規劃師考前複習班",
-      desc: "同步掌握考前趨勢，現場Q&A解析AI規劃師考題，名師親授！8/06(三) 20:00 線上直播",
-      btn: { text: "免費報名", url: "#", color: "success" }
-    },
-    {
-      title: "AI規劃師考前衝刺班",
-      desc: "強化策略，深度破解歷，提升考場自信！8/13(三) 20:00 線上直播",
-      btn: { text: "免費報名", url: "#", color: "success" }
-    }
-  ],
-  promo: [
-    {
-      title: "AI教師培育課程",
-      desc: "掌握AI知識體系，精通生成式AI技能，熟悉AI PC功能",
-      btn: { text: "立即了解",
-            url: "https://roc-central-ai-edu.org/uploads/page/Summer/", 
-            color: "warning" 
-           }
-    }
-  ],
-  history: [
-    {
-      title: "2025 AI規劃師考前複習班",
-      desc: "完整講解AI規劃師考試重點，逐題剖析常見失分陷阱。",
-      btn: { text: "觀看回放", url: "#", color: "outline-primary" }
-    },
-    {
-      title: "2025 AI規劃師考前衝刺班",
-      desc: "AI規劃師全覆蓋，掌握最新考情！",
-      btn: { text: "觀看回放", url: "#", color: "outline-primary" }
-    }
-  ]
-};
+
+  window.LECTURES_DATA = {
+    latest: [
+      {
+        title: "AI規劃師考前衝刺班",
+        desc: "強化策略，深度破解歷，提升考場自信！8/13(三) 20:00 線上直播",
+        btn: { text: "免費報名", url: "https://roc-central-ai-edu.org/modules/tadnews/index.php?ncsn=4&nsn=25", color: "success" }
+      }
+    ],
+    promo: [
+      {
+        title: "AI教師培育課程",
+        desc: "掌握AI知識體系，精通生成式AI技能，熟悉AI PC功能",
+        btn: {
+          text: "立即了解",
+          url: "https://roc-central-ai-edu.org/uploads/page/Summer/",
+          color: "warning"
+        }
+      }
+    ],
+    history: [
+      {
+        title: "2025 AI規劃師考前複習班",
+        desc: "完整講解AI規劃師考試重點，逐題剖析常見失分陷阱。",
+        btn: { text: "觀看回放", url: "#", color: "outline-primary" }
+      }
+    ]
+  };
+
+
